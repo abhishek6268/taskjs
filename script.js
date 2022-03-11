0812ee1d95b94ae2f5ac3418464fdbdb78f020d3
abhishek6268 <96810131+abhishek6268@users.noreply.github.com>
2022-03-11 21:45:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+ //display html tag used in the website or document
+var element;
+element = document;
+console.log(element);
+
+
+// return html element in the form of an array
+var element;
+element = document.all;
+console.log(element);
+
+// display a specific tag present in array at given index
+var element; 
+element = document.all[5];
+console.log(element);
+
+// display content present in head tag 
+var element;
+element = document.head;
+console.log(element);
+
+// display text present in title 
+var element;
+element = document.title;
+console.log(element);
+
+// display content present in  body
+var element;
+element = document.body;
+console.log(element);
+
+
+// display content in link form
+var element;
+element = document.links;
+console.log(element);
+
+// return images  present in web page in array form 
+var element;
+element = document.images;
+console.log(element);
+
+// returnforms  present in web page in array form 
+var element;
+element = document.forms;
+console.log(element);
+
+// return  the url of the website 
+var element;
+element = document.URL;
+console.log(element);
+
+// return content available in target id 
+var element;
+element = document.getElementById("demo");
+console.log(element);
+
+// return content available in target class id 
+var element;
+element = document.getElementsByClassName("navbar");
+console.log(element);
+
+// retrun content by targeting a specific tag
+var element;
+element = document.getElementsByTagName("h1");
+console.log(element);
+
+
+
